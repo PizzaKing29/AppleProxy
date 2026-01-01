a6c93254e1953ea6cf3edd5d281727b500fdc54c
PizzaKing29 <Legozap223@gmail.com>
2026-01-01 13:03:51 -0600

    Basic HTTP server

--- AppleProxy/Program.cs
+++ AppleProxy/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace AppleProxy;
+
+class Proxy
+{
+    public static void Main()
+    {
+        HttpListenRequests();
+    }
+
+    public static void HttpListenRequests()
+    {
+        try
+        {
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8001/");
+
+            httpListener.Start();
+            Console.WriteLine("Started listening for HTTP requests");
+
+            while (true)
+            {
+                httpListener.GetContext();
+                Console.WriteLine("Recieved HTTP Request");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error {e}");
+        }
+    }
+}
