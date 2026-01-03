924a248e612e7ad4322ea52565d153827b611a3b
PizzaKing29 <Legozap223@gmail.com>
2026-01-02 22:33:12 -0600

    Testing http GET requests

--- AppleProxy/Program.cs
+++ AppleProxy/Program.cs
@@ -24,11 +24,27 @@ class Proxy
             {
                 httpListener.GetContext();
                 Console.WriteLine("Recieved HTTP Request");
+                ReverseProxy();
             }
         }
         catch (Exception e)
         {
             Console.WriteLine($"Error {e}");
         }
+    }
+
+
+    public static void ReverseProxy()
+    {
+        // when program detects a http request on port 8001, GET http request stuff form port 8000
+        string baseUrl = "http://localhost:8000/";
+        HttpClient httpClient = new HttpClient();
+        byte[] data = new byte[10000];
+        Stream stream;
+
+        var request = httpClient.GetAsync(baseUrl);
+
+        Console.WriteLine("I think it worked");
+        Console.WriteLine($"Request Message: {request}");
     }
 }
